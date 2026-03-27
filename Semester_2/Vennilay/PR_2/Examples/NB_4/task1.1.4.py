857005f5c07905f704308ef1874b9b2bf4e5d3c5
vennilay <229026665+Vennilay@users.noreply.github.com>
2026-03-27 19:01:48 +0300

    Added PR_2

---
+++ Semester_2/Vennilay/PR_2/Examples/NB_4/task1.1.4.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+beta = (0.25, 0.75, 0.5)
+
+
+def f(x, b0, b1, b2):
+    return b0 + b1 * np.exp(-b2 * x ** 2)
+
+
+xdata = np.linspace(0, 5, 50)
+y = f(xdata, *beta)
+ydata = y + 0.05 * np.random.randn(len(xdata))
+
+beta_opt, beta_cov = curve_fit(f, xdata, ydata)
+lin_dev = sum(beta_cov[0])
+print(lin_dev)
+
+residuals = ydata - f(xdata, *beta_opt)
+fres = sum(residuals ** 2)
+print(fres)
+
+fig, ax = plt.subplots()
+ax.scatter(xdata, ydata)
+ax.plot(xdata, y, 'r', lw=2)
+ax.plot(xdata, f(xdata, *beta_opt), 'b', lw=2)
+ax.set_xlim(0, 5)
+ax.set_xlabel(r"$x$", fontsize=18)
+ax.set_ylabel(r"$f(x, \beta)$", fontsize=18)
+plt.show()
